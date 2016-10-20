4b11227f6750cb973cd5862d5ae89519ef878b93
Dedicatus <zjw7310@hotmail.com>
2016-10-21 00:22:39 +0800

    完成平方根函数

--- sqrt.scm
+++ sqrt.scm
@@ -1,1 +1,15 @@
-(+ 1 2)
+#lang racket
+
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
+
+(define (sqrt x)
+  (define (sqrtex guess last)
+    (if (< (abs (- guess last)) 0.000001)
+        guess
+        (sqrtex (improve guess) guess)))
+  (define (improve guess)
+    (/ (+ guess (/ x guess)) 2))
+  (sqrtex 1.0 2.0))
